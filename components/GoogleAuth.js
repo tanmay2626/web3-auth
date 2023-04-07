59ab958387108a639d9bc9edc573eca162dff8c1
tanmay2626~ <tanmaywaykar17@gmail.com>
2023-04-07 09:10:46 +0530

    Setup googauth using magic.link

--- components/GoogleAuth.js
+++ components/GoogleAuth.js
@@ -1,9 +1,26 @@
+import { magic } from '../libs/magic'
 import React from 'react'
 
 const Wallets = props => {
+
+  async function onSubmit() {
+    await magic.oauth.loginWithRedirect({
+      provider: 'google',
+      redirectURI: 'http://localhost:3000/callback', // required redirect to finish social login
+    });
+  }
+
   return (
     <div className='wallets'>
-    <button>Signin with Google</button>
+         <button
+              type='submit'
+              className='social-btn'
+              onClick={() => {
+                onSubmit();
+              }}
+            >
+              SignIn with Google
+     </button>
     </div>
   )
 }
---
+++ pages/callback.js
@@ -0,0 +1,42 @@
+import React,{ useEffect } from 'react'
+import { magic } from '../libs/magic';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const Callback = props => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        router.query.provider && finishSocialLogin();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [router.query]);
+
+    const finishSocialLogin = async () => {
+        let result = await magic.oauth.getRedirectResult();
+        authenticateWithServer(result.magic.idToken);
+      };
+
+      const authenticateWithServer = async (props) =>{
+        let userMetadata = await magic.user.getMetadata();
+         //returns email phone issuer(address)
+        const walletAddress = userMetadata.issuer.split(':')[2];
+        axios.post("http://localhost:8080/api/signin",{ walletAddress })
+        .then(res =>{
+          localStorage.setItem('token',`Bearer ${res.data.token}`);
+          router.push('/profile');
+        })
+        .catch(err =>{
+            console.log(`Error: ${err}`); 
+            router.push('/signin');
+        })
+      }
+
+  return (
+    <div>
+    <h1>Loading</h1>
+    </div>
+  )
+}
+
+export default Callback;
